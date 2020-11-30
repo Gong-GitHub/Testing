4ebc34a0f1f45c87810e4ec2dd05a6d57d6d18bb
Gong-GitHub <gzh_12@163.com>
2020-11-30 10:43:57 +0800

    base_page 初步封装

--- selenium1/selenium_qywx_main/page/main.py
+++ selenium1/selenium_qywx_main/page/main.py
@@ -1,17 +1,23 @@
 # Gong
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 from selenium1.selenium_qywx_main.page.add_menber import AddMenber
+from selenium1.selenium_qywx_main.page.base_page import BasePage
 
 
-class Main:
-    def __init__(self):
-        options = Options()
-        options.debugger_address = '127.0.0.1:9222'
-        self._driver = webdriver.Chrome(options=options)
-        self._driver.get('https://work.weixin.qq.com/wework_admin/frame')
+class Main(BasePage):
+    _base_url = 'https://work.weixin.qq.com/wework_admin/frame'
+
     def goto_add_member(self):
-        self._driver.find_element(By.XPATH,'//*[@id="_hmt_click"]/div[1]/div[4]/div[2]/a[1]/div/span[2]').click()
+        #self._driver.find_element(By.XPATH,'//*[@id="_hmt_click"]/div[1]/div[4]/div[2]/a[1]/div/span[2]').click()
+        self.find(By.XPATH,'//*[@id="menu_contacts"]/span').click()
+        locator=(By.CSS_SELECTOR,'.js_has_member>div:nth-child(1) a:nth-child(2)')
+        self.waif_for_click(locator)
+        self.find(By.CSS_SELECTOR,'.js_has_member>div:nth-child(1) a:nth-child(2)').click()
         return AddMenber(self._driver)
--- selenium1/selenium_qywx_main/test_page/test_add_member.py
+++ selenium1/selenium_qywx_main/test_page/test_add_member.py
@@ -1,4 +1,6 @@
 # Gong
+from time import sleep
+
 from selenium1.selenium_qywx_main.page.main import Main
 
 
@@ -6,6 +8,8 @@ class TestAddMember:
     def setup(self):
         self.main = Main()
     def test_addmenber(self):
-        self.main.goto_add_member().add_member()
+        add_member = self.main.goto_add_member()
+        add_member.add_member()
+        assert add_member.get_menber('9')
 
 
